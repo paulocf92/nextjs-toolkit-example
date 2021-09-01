6c44b38df2c35e089e206f3386c8302c1bfae0b2
Paulo Filho <pcf.feat@gmail.com>
2021-09-01 15:07:08 -0400

    chore: relay count info from redux to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
+import { useAppSelector } from '../app/hooks';
+import { selectCount } from '../features/counter/counterSlice';
+
 import { Counter } from '../components/Counter';
 import { KanyeQuote } from '../components/KanyeQuote';
 
 export default function Home() {
+  const count = useAppSelector(selectCount);
+
   return (
     <>
       <Counter />
+      <span
+        style={{
+          display: 'block',
+          padding: '10px 0',
+          color: '#3377ff',
+          fontFamily: 'sans-serif',
+          fontWeight: 'bold',
+        }}
+      >
+        Relaying count from Home: {count}
+      </span>
       <KanyeQuote />
     </>
-  )
+  );
 }
