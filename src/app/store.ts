7bf7c3b460dd7fd337fad006670ad59c63bd1e57
Paulo Filho <pcf.feat@gmail.com>
2021-09-01 14:42:33 -0400

    feat:add counter slice, effectively aggregating actions, reducers and selector in a single file

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,15 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import objectReducer from "./slices/objectSlice";
+import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
-  // This is where we add reducers.
-  // Eg. objectName: objectReducer
+    // This is where we add reducers.
+    counter: counterReducer,
   },
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 
-// Inferred type: {objectName: ObjectState}
+// Inferred type: {counter: CounterState}
 export type AppDispatch = typeof store.dispatch;
