c756b6fe2d45d6d4a4587b969c9f6c37cbf26b68
Paulo Filho <pcf.feat@gmail.com>
2021-09-01 15:01:59 -0400

    feat: apply redux toolkit management to the app

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,15 +1,46 @@
-export function Counter() {
-  return (
-    <div style={{ border: '2px solid #444', padding: 15, borderRadius: 3, width: '50%', marginBottom: 20}}>
-      <h1>Welcome to the awesome counter app!</h1>
-      <h2>The current number is 0</h2>
+import { useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  selectCount,
+} from '../../features/counter/counterSlice';
 
-      <input type="text" name="increaseBy" id="increaseBy" />
-      <button type="button">Increment by amount</button>
+export function Counter() {
+  const dispatch = useAppDispatch();
+  const count = useAppSelector(selectCount);
+  const [incrementAmount, setIncrementAmount] = useState<number>(0);
+
+  return (
+    <div
+      style={{
+        border: '2px solid #444',
+        padding: 15,
+        borderRadius: 3,
+        width: '50%',
+        marginBottom: 20,
+      }}
+    >
+      <h1>Welcome to the awesome counter app!</h1>
+      <h2>The current number is {count}</h2>
+
       <div>
-        <button type="button">Decrement by 1</button>
-        <button type="button">Increment by 1</button>
+        <input
+          value={incrementAmount}
+          onChange={e => setIncrementAmount(Number(e.target.value))}
+          type='number'
+        />
+        <button
+          onClick={() => dispatch(incrementByAmount(Number(incrementAmount)))}
+        >
+          Increment by amount
+        </button>
+      </div>
+      <div>
+        <button onClick={() => dispatch(decrement())}>Decrement by 1</button>
+        <button onClick={() => dispatch(increment())}>Increment by 1</button>
       </div>
     </div>
-  )
+  );
 }
